2fea183bea2852c779f00900cb05eeca53db7df0
dnwls1133 <117172889+dnwls1133@users.noreply.github.com>
2025-05-21 01:07:15 +0900

    Winapi Engine 수정본
    수정

---
+++ Engine/Unity/unity_2AAPLDL88VFIUVXM.cpp
@@ -0,0 +1,24 @@
+
+#include "C:\Users\dnwls\Documents\GitHub\WinapiEngine\Engine\CAnimation.cpp"
+
+
+#include "C:\Users\dnwls\Documents\GitHub\WinapiEngine\Engine\CAnimator.cpp"
+
+
+#include "C:\Users\dnwls\Documents\GitHub\WinapiEngine\Engine\CBackground.cpp"
+
+
+#include "C:\Users\dnwls\Documents\GitHub\WinapiEngine\Engine\CBtnUI.cpp"
+
+
+#include "C:\Users\dnwls\Documents\GitHub\WinapiEngine\Engine\CCamera.cpp"
+
+
+#include "C:\Users\dnwls\Documents\GitHub\WinapiEngine\Engine\CCollider.cpp"
+
+
+#include "C:\Users\dnwls\Documents\GitHub\WinapiEngine\Engine\CColliderMgr.cpp"
+
+
+#include "C:\Users\dnwls\Documents\GitHub\WinapiEngine\Engine\CCore.cpp"
+
---
+++ Engine/Unity/unity_2L1T8IDIBLKD9698.cpp
@@ -0,0 +1,24 @@
+
+#include "C:\Users\dnwls\Documents\GitHub\WinapiEngine\Engine\CSound.cpp"
+
+
+#include "C:\Users\dnwls\Documents\GitHub\WinapiEngine\Engine\CTexture.cpp"
+
+
+#include "C:\Users\dnwls\Documents\GitHub\WinapiEngine\Engine\CTile.cpp"
+
+
+#include "C:\Users\dnwls\Documents\GitHub\WinapiEngine\Engine\CTimeMgr.cpp"
+
+
+#include "C:\Users\dnwls\Documents\GitHub\WinapiEngine\Engine\CUI.cpp"
+
+
+#include "C:\Users\dnwls\Documents\GitHub\WinapiEngine\Engine\CUIMgr.cpp"
+
+
+#include "C:\Users\dnwls\Documents\GitHub\WinapiEngine\Engine\Engine.cpp"
+
+
+#include "C:\Users\dnwls\Documents\GitHub\WinapiEngine\Engine\func.cpp"
+
